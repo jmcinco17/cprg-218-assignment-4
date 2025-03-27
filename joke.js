7bccef9c11eee35ef3044cd56b929a4fc1ebe805
John <johnmark.cinco@edu.sait.ca>
2025-03-26 23:25:22 -0600

    feat: add all jokes

--- joke.js
+++ joke.js
@@ -1,10 +1,15 @@
-function fetchjoke(){
+function fetchjoke(category ='Any'){
 
- fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
- .then(response => response.json())
- .then(data => {
+fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
+.then(response => response.json())
+.then(data => {
     console.log(data);
-    document.getElementById('programming').textContent = data.joke;
-    });
+    document.getElementById('joke').textContent = data.type === "twopart" 
+    ? `${data.setup} ... ${data.delivery}` :data.joke;
+    document.getElementById('title').textContent = 
+    category === 'Any' ? "Joke Of The Day" 
+    : category === 'Pun' ? "A Random Pun" 
+    : `A Random ${category} Joke`;
 
+});
 }
